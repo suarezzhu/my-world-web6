2e85e7f3b67964c0b6c58586eef10561e8cccbb6
suarezzhu <zxc2511836601@gmail.com>
2023-01-14 18:25:07 +0800

    初始化

---
+++ vite.config.ts
@@ -0,0 +1,30 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { resolve } from "path";
+const pathResolve = (dir: string) => resolve(__dirname, dir);
+
+export default defineConfig({
+  plugins: [
+    vue()
+  ],
+  resolve: {
+    alias: {
+      // 这里不需要__dirname，直接使用，因为vite3内置了这个变量，用vite2的方式会报错
+      "@": resolve("src"),
+      "views": resolve("src/views"),
+      'vue-i18n': 'vue-i18n/dist/vue-i18n.cjs.js',
+    },
+  },
+
+  // 这里是代理配置，如果后端设置了跨域，这里可以直接删掉
+  server: {
+    proxy: {
+      '/api': {
+        target: "http://127.0.0.1:8000/",
+        changeOrigin: true
+      }
+    }
+  }
+
+
+})
